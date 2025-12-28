8971636f9997ef21193d739299deecb7e0d92681
lukaarellano <arellanoluka76+git@gmail.com>
2025-12-27 21:14:23 -0600

    Gravity should change with altitude

--- rocket.py
+++ rocket.py
@@ -8,32 +8,35 @@ class Rocket:
         self.isp = isp
         self.burn_time = burn_time
     def get_deltav(self):
-        self.deltav = float(math.log(self.mass/(self.mass-self.fuel))) * 9.81 * float(self.isp)
+        self.deltav = float(math.log(self.mass/(self.mass - self.fuel))) * 9.81 * float(self.isp)
         return self.deltav
     def get_thrust(self):
         self.mass_flow_rate = self.fuel / self.burn_time
         self.thrust = self.mass_flow_rate * self.isp * 9.81
         return self.thrust, self.mass_flow_rate
     def ignite_engines(self):
+        self.initial_gravity = 9.81
+        self.height = 0
         self.acceleration = 0
         self.velocity = 0
-        self.height = 0
-        self.initial_acceleration = self.thrust/self.mass - 9.81
+        self.initial_acceleration = self.thrust/self.mass - self.initial_gravity
         print("Engines ignited!")
         while self.fuel > 1:
             self.mass = self.mass - self.mass_flow_rate
+            self.gravity = self.initial_gravity * (6371000/(6371000+self.height))**2
             self.fuel = self.fuel - self.mass_flow_rate
-            self.deltav = float(math.log(self.mass/(self.mass-self.fuel))) * 9.81 * float(self.isp)
-            self.acceleration = self.thrust/self.mass - 9.81
+            self.deltav = float(math.log(self.mass/(self.mass-self.fuel))) * self.gravity * float(self.isp)
+            self.acceleration = self.thrust/self.mass - self.gravity
             self.average_acceleration = (self.initial_acceleration + self.acceleration) / 2
             self.velocity += self.average_acceleration
             self.average_velocity = (self.deltav / 2)
             self.height += self.velocity
             print("DeltaV: " + str(round(self.deltav)) + " m/s.")
             print("Velocity: " + str(round(self.velocity)) + " m/s.")
-            print("Height: " + str(round(self.height)) + " meters.")
+            print("Height: " + str(self.height) + " meters.")
+            print("Gravity: " + str(round(self.gravity)) + " m/s.")
             print("------------------------------------")
             sleep(1)
-        self.apoapsis = self.height + ((self.velocity * self.velocity) / (9.81 * 2))
+        self.apoapsis = self.height + ((self.velocity * self.velocity) / (self.gravity * 2))
         print("Apoapsis: " + str(round(self.apoapsis)) + " meters.")
         print("Fuel depleted")
